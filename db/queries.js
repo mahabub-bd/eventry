bf0720f5ab1a9310c8782e9eedbc62d087de980f
Mahabub Hossain <palashmahabub@gmail.com>
2024-05-02 18:11:23 +0600

    add event

--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,18 @@
 import { eventModel } from "@/models/event-models";
+import { dbConncect } from "@/services/mongo";
 import {
   replaceMongoIdInArray,
   replaceMongoIdInObject,
 } from "@/utils/data-util";
 
 async function getAllEvents() {
+  await dbConncect();
   const allEvents = await eventModel.find().lean();
   return replaceMongoIdInArray(allEvents);
 }
 
 async function getEventById(eventId) {
+  await dbConncect();
   const event = await eventModel.findById(eventId).lean();
   return replaceMongoIdInObject(event);
 }
